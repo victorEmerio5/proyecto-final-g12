3647813f24a62d6f5aef7240ca42ff91499f599d
victor emerio <victoremerio12@gmail.com>
2022-11-12 16:51:08 -0500

    implementando props en el componente Intro

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ function App() {
     <div className="App">
      <h1>SERVICIO DE CITAS DE VETERINARIA</h1>
      <div className='cont-princ'>
-       <Intro />
+       <Intro 
+         titulo='Mision:'
+         descripcion='Servicio de citas de veterinaria es una empresa que promueve el bienestar de todo tipo de mascota. Nuestro principal objetivo es ofrecer a nuestros clientes las herramientas neceasarias para que puedan acceder a nuestros servicios desde la comodidad de su casa mediante el agendamiento de citas veterinarias las 24 horas del dia.Dentro de nuestro portafolio de servicios tenemos asistencia, cuidados, nutricion, inmunizacion y esterilizacion. De esta manera garantizamos el confort de nuestros clientes y la salud de sus mascotas.'
+        />
      </div>
     </div>
   );
--- src/componentes/Intro.jsx
+++ src/componentes/Intro.jsx
@@ -3,13 +3,13 @@ import '../hojas-estilos/Intro.css';
 
 // componente Intro
 
-function Intro (){
+function Intro (props){
   return(
     <div className='cont-intro'>
       <img className='img-intro' src={require('../imagenes/tonete.png')} alt='imagen mascota' />
       <div className='cont-texto-intro'>
-        <p className='mision'>Mision:</p>
-        <p className='texto-mision'>Servicio de citas de veterinaria es una empresa que promueve el bienestar de todo tipo de mascota. Nuestro principal objetivo es ofrecer a nuestros clientes las herramientas neceasarias para que puedan acceder a nuestros servicios desde la comodidad de su casa mediante el agendamiento de citas veterinarias las 24 horas del dia.Dentro de nuestro portafolio de servicios tenemos asistencia, cuidados, nutricion, inmunizacion y esterilizacion. De esta manera garantizamos el confort de nuestros clientes y la salud de sus mascotas. </p>
+        <p className='mision'>{props.titulo}</p>
+        <p className='texto-mision'>{props.descripcion}</p>
       </div>
     </div>
   );
