173568767aabbdd71271763487692869828f2e78
victor emerio <victoremerio12@gmail.com>
2022-11-13 18:00:23 -0500

    implementando navbar

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import Intro from './componentes/Intro.jsx';
+import Intro from './intro/Intro.jsx';
 
 //componente principal del programa
 
@@ -8,12 +8,12 @@ function App() {
   return (
     <div className="App">
       <h1>SERVICIO DE CITAS DE VETERINARIA</h1>
-        <div className='cont-princ'>
-          <Intro 
-          titulo='Mision:'
-          descripcion='Servicio de citas de veterinaria es una empresa que promueve el bienestar de todo tipo de mascota. Nuestro principal objetivo es ofrecer a nuestros clientes las herramientas neceasarias para que puedan acceder a nuestros servicios desde la comodidad de su casa mediante el agendamiento de citas veterinarias las 24 horas del dia.Dentro de nuestro portafolio de servicios tenemos asistencia, cuidados, nutricion, inmunizacion y esterilizacion. De esta manera garantizamos el confort de nuestros clientes y la salud de sus mascotas.'
-          />
-        </div>
+      <div className='cont-princ'>
+        <Intro 
+        titulo='Mision:'
+        descripcion='Servicio de citas de veterinaria es una empresa que promueve el bienestar de todo tipo de mascota. Nuestro principal objetivo es ofrecer a nuestros clientes las herramientas neceasarias para que puedan acceder a nuestros servicios desde la comodidad de su casa mediante el agendamiento de citas veterinarias las 24 horas del dia.Dentro de nuestro portafolio de servicios tenemos asistencia, cuidados, nutricion, inmunizacion y esterilizacion. De esta manera garantizamos el confort de nuestros clientes y la salud de sus mascotas.'
+        />
+      </div>
     </div>
   );
 }
---
+++ src/FormatoCitas/FormatoCitas.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+//componente FormatoCitas
+
+const FormatoCitas = () => {
+  return (
+    <div>FormatoCitas</div>
+  )
+}
+
+export default FormatoCitas
