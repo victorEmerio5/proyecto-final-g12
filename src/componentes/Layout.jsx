70daadec940193b1867aacaabca9d5571ca26cc8
victor emerio <victoremerio12@gmail.com>
2022-11-22 21:03:38 -0500

    cambiando estilos

--- src/componentes/Layout.jsx
+++ src/componentes/Layout.jsx
@@ -6,16 +6,18 @@ import {Outlet, Link} from 'react-router-dom';
 const Layout = () => {
   return (
     <>
-      <nav>
-        <ul>
-          <li>
-            <Link to='/'>Home</Link>
-          </li>
-          <li>
-            <Link to='/tramites'>Tramites</Link>
-          </li>
-        </ul>
-      </nav>
+      <div className='cont-navBar'>
+        <nav>
+          <ul>
+            <li>
+              <Link to='/'>Home</Link>
+            </li>
+            <li>
+              <Link to='/tramites'>Tramites</Link>
+            </li>
+          </ul>
+        </nav>
+      </div>
 
       <Outlet/>
     </>
